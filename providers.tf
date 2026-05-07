b293a45e51d1c014de009a1248f8360c3f42744a
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-07 19:50:33 +0200

    fix infraestructura

--- providers.tf
+++ providers.tf
@@ -20,8 +20,8 @@ terraform {
 provider "azurerm" {
   features {}
 
+  use_oidc        = true
   client_id       = var.client_id
-  client_secret   = var.client_secret
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
