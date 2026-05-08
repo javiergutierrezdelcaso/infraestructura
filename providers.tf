6d301014f9e3e76005b7f40aae08fa9e5e810203
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-08 20:36:16 +0200

    Fix

--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.104.0"
+      version = "~> 3.100"
     }
   }
 
@@ -12,11 +12,16 @@ terraform {
     organization = "ecoanalyzer-org"
 
     workspaces {
-      name = "ecoanalyzer-infra"
+      name = "Infraestructura"
     }
   }
 }
 
 provider "azurerm" {
   features {}
+
+  use_oidc        = true
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
