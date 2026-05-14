03cd6fb2b9929a4f3b716881130dc605ead760c4
Javier Gutiérrez Del Caso <javier.gutierrezd@alumnos.viu.es>
2026-05-14 18:31:44 +0200

    cambio outpus.tf

--- terraform/modules/app_service/ouputs.tf
+++ terraform/modules/app_service/ouputs.tf
@@ -1,11 +1,11 @@
 output "app_url" {
-  value = azurerm_linux_web_app.app.default_hostname
+  value = try(azurerm_linux_web_app.app.default_hostname, null)
 }
 
 output "app_id" {
-  value = azurerm_linux_web_app.app.id
+  value = try(azurerm_linux_web_app.app.id, null)
 }
 
 output "plan_id" {
-  value = azurerm_service_plan.plan.id
+  value = try(azurerm_service_plan.plan.id, null)
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "pre_app_url" {
-  value       = module.app_service_pre[0].app_url
+  value       = try(module.app_service_pre[0].app_url, null)
   description = "URL del App Service PRE"
 }
 
 output "pro_app_url" {
-  value       = module.app_service_pro[0].app_url
+  value       = try(module.app_service_pro[0].app_url, null)
   description = "URL del App Service PRO"
 }
